29418bb1bdd7f9eb12a4576ab961d96e958e5bd7
AlyonaAg <74561013+AlyonaAg@users.noreply.github.com>
2021-06-18 23:43:59 +0300

    Add files via upload

---
+++ Scripts/script7.js
@@ -0,0 +1,8 @@
+document.getElementById("demo").innerHTML = 
+typeof "" + "<br>" +
+typeof "John" + "<br>" + 
+typeof "John Doe" + "<br>" +
+typeof 0 + "<br>" +
+typeof 314 + "<br>" +
+typeof 3.14 + "<br>" +
+typeof (3.14);
